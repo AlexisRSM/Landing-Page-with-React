add681ae77a3c6bf1017628f918697103795f3a3
AlexisRSM <alexismendes98@hotmail.com>
2024-06-05 19:58:10 +0100

    V0.1

---
+++ src/Footer.jsx
@@ -0,0 +1,90 @@
+function Footer() {
+    return (
+        <div class="bg-dark text-light pt-5 pb-4">
+            <div class="container text-center text-md-left">
+                <div class="row text-center text-md-left">
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+                        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Quick Links</h5>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Home</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">About</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Services</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Contact</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Useful links</h5>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Your Account</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Become an Affiliate</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Shipping Rates</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-light" style="text-decoration: none;">Help</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
+                        <p>
+                            <i class="fas fa-home mr-3"></i> New York, NY 2333, US
+                        </p>
+                        <p>
+                            <i class="fas fa-envelope mr-3"></i> info@example.com
+                        </p>
+                        <p>
+                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88
+                        </p>
+                        <p>
+                            <i class="fas fa-print mr-3"></i> + 01 234 567 89
+                        </p>
+                    </div>
+                </div>
+                <hr class="mb-4"></hr>
+                <div class="row align-items-center">
+                    <div class="col-md-7 col-lg-8">
+                        <p class="text-center text-md-left">© 2023 Copyright:
+                            <a href="#" class="text-warning" style="text-decoration: none;">
+                                <strong> YourCompany.com</strong>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-md-5 col-lg-4">
+                        <div class="text-center text-md-right">
+                            <ul class="list-unstyled list-inline">
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-light" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-light" style="font-size: 23px;"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-light" style="font-size: 23px;"><i class="fab fa-google-plus"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-light" style="font-size: 23px;"><i class="fab fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
